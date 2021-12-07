1e87cf6d33dadc11c67e4f33c9153a08e733784d
Biswajit <biswajit.majumdar@carnationit.com>
2021-12-07 20:45:20 +0530

    Hashing added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const user = require('./models/user')
+const bcrypt = require('bcryptjs')
 
 mongoose.connect('mongodb://localhost:27017/login-app-db')
 
@@ -10,8 +11,15 @@ const app = express()
 app.use('/',express.static(path.join(__dirname,'static')))
 app.use(bodyParser.json())
 
+bcrypt
+
 app.post('/api/register',async (req,res)=>{
     console.log(req.body)
+    // Analysts
+    //open API
+
+    console.log(await bcrypt.hash(req.body.password,10))
+    // Hashing the passwords
     res.json({status:'ok'})
 })
 
